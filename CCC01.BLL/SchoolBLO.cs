b09b8bf203090e62ee2b92244bc8cfb1d2b65cef
Willy-joël TCHANA <willyjoeltchana@gmail.com>
2020-12-08 11:16:09 +0100

    Add Save and GetAll school methods in SchoolBLO

---
+++ CCC01.BLL/SchoolBLO.cs
@@ -0,0 +1,29 @@
+﻿using CCC01.BO;
+using CCC01.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC01.BLL
+{
+    public class SchoolBLO
+    {
+        private readonly Repository<School> repository;
+        public SchoolBLO()
+        {
+            repository = new Repository<School>("Data");
+        }
+
+        public void Save(School school)
+        {
+            repository.Add(school);
+        }
+
+        public IEnumerable<School> GetAllSchool()
+        {
+            return repository.Find();
+        }
+    }
+}
